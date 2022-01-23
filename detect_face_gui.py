e68cc1f99839f807ad59f5e4ab4287b37f20a600
tw1sted1981 <tw1sted_an1mator@hotmail.com>
2022-01-23 19:52:43 +0000

    copy crop data button working now

--- detect_face_gui.py
+++ detect_face_gui.py
@@ -5,7 +5,6 @@ import logging
 from pprint import pprint
 from collections import defaultdict
 
-
 from Qt import QtWidgets, QtGui, QtCore, QtCompat
 
 from mtcnn import MTCNN
@@ -24,33 +23,22 @@ class MyApp():
 
         self.execute_folder = os.path.dirname(__file__)
         self.sequence_directory = ''
+        self.animated_crop = ''
 
+        self.app = QtCompat.loadUi(path_ui)
 
+        my_app = QtWidgets.QApplication.instance()
+        self.clipboard = my_app.clipboard()
 
-        self.app = QtCompat.loadUi(path_ui)  
-        
-        #self.clipboard = self.app.clipboard()
         # set default values      
         self.app.lbl_folder_directory.setText(self.sequence_directory)
         self.app.lbl_face.setPixmap(QtGui.QPixmap(path_img.format('lbl_face')))
 
         # create connections
         self.app.btn_select_folder.clicked.connect(self.press_btnSelectFolder)
-        
-        #self.thread = QtCore.QThread()
-        #self.worker = Worker(self.sequence_directory)
-        #self.worker.moveToThread(self.thread)
-        #self.app.btn_findFaces.clicked.connect(self.worker.findFaces)    
-
-        
-        
         self.app.btn_findFaces.clicked.connect(self.findFaces)
-        
         self.app.btn_copy_to_clipboard.clicked.connect(self.press_btn_copy_to_clipboard)
-        
-        
 
-        
         self.app.show()
 
     def press_btnSelectFolder(self):
@@ -72,20 +60,19 @@ class MyApp():
                 self.list.addItem(str(sequence))  
 
     def press_btn_copy_to_clipboard(self): 
-        pass
-        #self.app.clipboard().setText(str(animated_crop)) 
-
+        self.clipboard.setText(str(self.animated_crop))
+        print('copy button clicked')
     
     def findFaces(self):
         face = loader.FrameHandler(self.sequence_directory) 
         id_of_sequence = (self.app.sequences_list.currentRow())
         face.select_sequence(id_of_sequence)
         print(self.app.sequences_list.currentItem())
+
         # create the detector, using default weights
         detector = MTCNN()
 
-        start_time = time.time()
-
+        start_time      = time.time()
         first_frame     = face.first_frame()
         last_frame      = face.last_frame()
 
@@ -130,8 +117,6 @@ class MyApp():
         # create animated curve
 
         x1 = y1 = x2 = y2 = ''
-
-        #sorted_dict = dict(sorted(nuke_animation.items()))
         for frame in range(first_frame, last_frame+1,):
             logging.info(frame)
             x1 += nnb.create_frame_value(frame, nuke_animation[frame]['x1'])
@@ -145,18 +130,11 @@ class MyApp():
         y2_anim = nnb.create_animation_curve(y2)
 
         animation_curve_list = [x1_anim, y1_anim, x2_anim, y2_anim]
-        animated_crop =nnb.create_crop(animation_curve_list)
-        #print(animated_crop)
+        self.animated_crop =nnb.create_crop(animation_curve_list)
+        print(self.animated_crop)
         log_msg = 'Total time to process was {} '.format(time.time() - start_time)
         self.app.txt_statusLog.appendPlainText(str(log_msg))
-        #log_msg = 'Nuke Crop Node \n\n{} '.format(animated_crop)
-        #self.app.txt_statusLog.appendPlainText(str(log_msg))
-
-        self.app.lbl_crop.setPlainText(str(animated_crop))
-
-        
-    
-
+        self.app.lbl_crop.setPlainText(str(self.animated_crop))
 
 def main():  
     app = QtWidgets.QApplication(sys.argv)
