816c354e56dd0e7b5a0a607a77aaef3272be3756
tw1sted1981 <tw1sted_an1mator@hotmail.com>
2022-01-24 00:28:56 +0000

    adding the assignment folder and readme.txt summarising what I have done

---
+++ 04_ui/assignment/assignment_ui.py
@@ -0,0 +1,78 @@
+# UI ***************************************************************************
+# content = assignment (Python Advanced)
+#
+# deliver = Upload the files to your (private) git repository
+#           and share it in the assignment submission form.
+#
+# date    = 2021-11-07
+# email   = alexanderrichtertd@gmail.com
+#*******************************************************************************
+"""
+
+
+
+
+#*******************************************************************************
+# 01. PREPARE
+#*******************************************************************************
+Install PySide2 and download Qt.py
+
+TIP: Check out pip!
+https://pypi.org/project/pip
+
+
+
+#*******************************************************************************
+# 02. RECREATE arUtil
+*******************************************************************************
+Check out the simpleUI.py module.
+Rebuild a simple UI by creating simpleUI.ui in Designer and connect it to simpleUI.py.
+The code in simpleUI.py is final. Build the UI and take a screenshot.
+
+
+
+
+
+#*******************************************************************************
+# 03. REFERENCE app
+#*******************************************************************************
+Choose an image reference of a tool (best to pick a DCC plugin)
+and reconstruct the UI in the designer. Make sure to use layouts.
+
+Add (real or pseudo) functionalities to the tool.
+
+NOTE: A great opportunity to play with all the different widgets and connections.
+
+
+
+
+
+#*******************************************************************************
+# 04. CREATE UI
+#*******************************************************************************
+Create a UI for your application via code or designer.
+
+Use PyQt5, PySide2 or Qt.py (whatever works best for you).
+Use buttons, checkboxes, sliders, edits, colors, layout … the more the better.
+Make sure that your UI is executable in OS and inside a DCC (e.g. Maya).
+Follow the UI design rules, make it pretty and make a screenshot.
+
+Record a one-minute-video (with or without voice over)
+in which you show the app's functionalities in a real life scenario.
+Make sure it "sells" your app to the audience aka "How does it help the production?"
+
+BONUS: Integrate the app into your DCC (shelf, toolbar, menu, ...)
+
+
+
+
+
+
+#*******************************************************************************
+# 05. REFLECTION
+#*******************************************************************************
+Make sure to reflect on your progress by filling out the form:
+https://forms.gle/52zpabbFu5EuQqk98
+
+
+"""
---
+++ 04_ui/assignment/readme.txt
@@ -0,0 +1,20 @@
+01 PySide2 and Qt.py installed via PIP.  Using Anaconda to manage my python 
+environments 
+
+02 Created a simple UI and tested the buttons run the 2 functions.
+
+03 This section I just followed your videos and got familiar with the designer
+I wanted to focus my efforts on my tool so really just spent my time following
+to understand better what does what in QT Designer.
+
+04 Created a UI for my application and wrote the application during the last 4
+weeks.  Had to learn about setting up my python environments and test and select
+modules I felt solved the problem best.  I built some tools to assist in creating
+nuke node structures from scratch.  This tool is at the root of this repo.
+
+A video was created to show it working.  It is not integrated within Nuke due
+to the libaries in use and more than liekly not compiled to the same version
+that nuke 13 is using.
+
+
+05 Form filled in :-D
